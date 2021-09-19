0864bf2d9358f80929a57ab66099173f8f5b7529
Mohammed Mohiuddin <mohibdrs@gmail.com>
2021-09-19 18:19:14 +0600

    route

--- routes/post_route.js
+++ routes/post_route.js
@@ -9,7 +9,7 @@ router.get("/", (req, res) => {
 });
 
 router.post(
-  "/signup",
+  "/user",
   catchAsync(async (req, res, next) => {
     const newUser = new User(req.body);
     await newUser.save();
@@ -18,6 +18,36 @@ router.post(
     // res.json(newUser);
   })
 );
+// User_Show
+router.get('/user_show/:id', async(req, res) => {
+  const {id} = req.params;
+  const user = await User.findById({id});
+  res.render("user_show", {user})
+})
+
+// User Show
+router.get('/create_post/:id', async(req, res)=> {
+  const {id} = req.body
+  const users = await User.findById({id});
+  res.render("post", {users})
+})
+
+router.post("/user/:id/posts", catchAsync(async(req, res, next) => {
+const {id} = req.params;  
+const user = await User.findById(id);
+const { title, caption, image, comment } = req.body;
+const post = new Post({title, caption, image, comment});
+user.posts.push(post);
+post.user = user;
+await user.save();
+await post.save();
+res.redirect(`/user/${id}`)
+}));
 
 
+router.get("/users", async(req, res) => {
+  const users = await User.find({});
+    res.render("user.ejs", {users});
+  });
+  
 module.exports = router;
